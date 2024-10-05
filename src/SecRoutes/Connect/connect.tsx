6568e05d52f772f3fcc5f96af948e4c6d25c4753
janewise <janewise2001@gmail.com>
2024-10-05 19:21:27 +0630

    wallet update9

--- src/SecRoutes/Connect/connect.tsx
+++ src/SecRoutes/Connect/connect.tsx
@@ -305,10 +305,25 @@ export function Connect() {
     }
   };
 
-  //shorten
+  //shorten walletadress
   const shortenAddress = (address: string) => {
-    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+    return `${address.slice(0, 7)}...${address.slice(-5)}`;
   };
+//copy walletadress
+const handleCopy = () => {
+  if (connectedWallet) {
+    navigator.clipboard.writeText(connectedWallet)
+      .then(() => {
+        alert('Address copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Failed to copy: ', err);
+      });
+  } else {
+    console.error('No wallet address to copy.');
+  }
+};
+
 
   // Function to handle disconnect
   const handleDisconnect = async () => {
@@ -360,7 +375,12 @@ export function Connect() {
             </form>
           ) : (
             <div className="wallet-connected">
-              <h5>  {shortenAddress(connectedWallet)}</h5>
+              <h5>  {shortenAddress(connectedWallet)}
+              <span onClick={handleCopy} className="providewalleticon"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+</svg>
+</span>
+              </h5>
               <button className="disconnectbutton" onClick={handleDisconnect}>
                 Disconnect
               </button>
